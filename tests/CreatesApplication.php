b3d3b1cb9ae877315ffc4feb061a2ad471d33755
刘荣 <ckryo@CkryodeMacBook-Pro.local>
2018-02-22 15:00:12 +0800

    first commit

---
+++ tests/CreatesApplication.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Contracts\Console\Kernel;
+
+trait CreatesApplication
+{
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require(__DIR__.'/../laravel/app.php');
+
+        $app->make(Kernel::class)->bootstrap();
+
+        Hash::driver('bcrypt')->setRounds(4);
+
+        return $app;
+    }
+}
